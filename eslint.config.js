81b152e52201f25cdf39d7a885d6009693652ec3
ThatGuySam <sam@sam.lc>
2025-02-16 16:29:29 -0600

    update: tweak rules to clean orval objects

--- eslint.config.js
+++ eslint.config.js
@@ -5,15 +5,85 @@ const DEFAULT_SPACE_INDENT = 4
 
 export default antfu(
     {
-        type: 'lib',
-        yaml: false,
+        type     : 'lib',
+        yaml     : false,
         stylistic: {
-            indent: DEFAULT_SPACE_INDENT,
+            indent   : DEFAULT_SPACE_INDENT,
+            overrides: {
+                '@stylistic/max-len': [
+                    'error',
+                    {
+                        code: 60,
+                    },
+                ],
+                '@stylistic/object-curly-newline': [
+                    'error',
+                    {
+                        ObjectExpression: {
+                            multiline    : true,
+                            minProperties: 1,
+                        },
+                        ObjectPattern: {
+                            multiline    : true,
+                            minProperties: 1,
+                        },
+                    },
+                ],
+                '@stylistic/object-property-newline': [
+                    'error',
+                    {
+                        allowAllPropertiesOnSameLine: false,
+                    },
+                ],
+                '@stylistic/key-spacing': [
+                    'error',
+                    {
+                        beforeColon: false,
+                        afterColon : true,
+                        align      : 'colon',
+                    },
+                ],
+                '@stylistic/indent-binary-ops': [
+                    'error',
+                    4,
+                ],
+                '@stylistic/array-bracket-newline': [
+                    'error',
+                    {
+                        multiline: true,
+                        minItems : 2,
+                    },
+                ],
+                '@stylistic/array-element-newline': [
+                    'error',
+                    {
+                        multiline: true,
+                        minItems : 2,
+                    },
+                ],
+                '@stylistic/array-bracket-spacing': [
+                    'error',
+                    'never',
+                ],
+                '@stylistic/comma-style': [
+                    'error',
+                    'last',
+                ],
+                '@stylistic/comma-spacing': [
+                    'error',
+                    {
+                        before: false,
+                        after : true,
+                    },
+                ],
+                '@stylistic/comma-dangle': [
+                    'error',
+                    'always-multiline',
+                ],
+            },
         },
     },
     {
-        ignores: [
-            'openapi.yaml',
-        ],
+        ignores: ['openapi.yaml'],
     },
 )
