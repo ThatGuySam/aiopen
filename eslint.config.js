1309b2a022731e2ac07c3ed3bd3e421707ae1543
ThatGuySam <sam@sam.lc>
2025-02-16 15:56:40 -0600

    update: set indent to 4 spaces

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
+const DEFAULT_SPACE_INDENT = 4
+
 export default antfu(
-  {
-    type: 'lib',
-  },
-  {
-    ignores: [
-      'openapi.yaml',
-    ],
-  },
+    {
+        type: 'lib',
+        stylistic: {
+            indent: DEFAULT_SPACE_INDENT,
+        },
+    },
+    {
+        ignores: [
+            'openapi.yaml',
+        ],
+    },
 )
