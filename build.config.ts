171a3a1549bcc94c7324e813098f15a1ea50a107
ThatGuySam <sam@sam.lc>
2025-02-15 18:27:26 -0600

    update: configure build for msw entry point

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,23 @@
+import { resolve } from 'pathe'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    'src/index',
+    { input: 'src/index', name: 'index' },
   ],
   declaration: 'node16',
   clean: true,
+  alias: {
+    '@': resolve(__dirname, './src'),
+  },
+  rollup: {
+    emitCJS: false,
+    esbuild: {
+      minify: true,
+      target: 'node18',
+    },
+    output: {
+      banner: '#!/usr/bin/env node',
+    },
+  },
 })
